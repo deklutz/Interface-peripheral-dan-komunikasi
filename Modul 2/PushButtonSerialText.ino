450806efe5fee1a2505ec43c815c9d8e7f22f701
Hanindita Rifdah <37829261+deklutz@users.noreply.github.com>
2023-10-19 12:30:35 +0700

    Update PushButtonSerialText.ino

--- Modul 2/PushButtonSerialText.ino
+++ Modul 2/PushButtonSerialText.ino
@@ -1,10 +1,14 @@
 #include <LiquidCrystal_I2C.h> // Library yang digunakan
 LiquidCrystal_I2C lcd(0x27, 16, 2);  // set alamat LCD menjadi 0x27 untuk display 16x2
+int tombol = 5;
 
 void setup(){ 
+  pinMode(tombol, INPUT);
   lcd.init();
   lcd.backlight();  // Make sure backlight is on
   lcd.begin(16, 2);
+  lcd.setCursor(2, 0);  //Set cursor to character 2 on line 0
+  lcd.print("Hello world!");
   Serial.begin(9600); 
 } 
 
