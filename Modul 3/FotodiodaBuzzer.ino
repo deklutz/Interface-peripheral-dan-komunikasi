d79998edf866b0c84b12a4e8fe7735f4e7f74161
Hanindita Rifdah <37829261+deklutz@users.noreply.github.com>
2023-10-19 13:38:57 +0700

    Create FotodiodaBuzzer.ino

---
+++ Modul 3/FotodiodaBuzzer.ino
@@ -0,0 +1,24 @@
+int buzz = 10; 
+int senRead = A1; 
+int limit = 850;
+int pd = A1
+
+void setup() { 
+ pinMode(pd,OUTPUT); 
+pinMode(buzz,OUTPUT); 
+digitalWrite(buzz,LOW); 
+Serial.begin(9600); 
+} 
+
+void loop() { 
+  int val=analogRead(pd); 
+  Serial.println(val); 
+  if(val <= limit) { 
+  digitalWrite(buzz,HIGH); 
+  delay(20); 
+   } 
+  else if(val > limit) { 
+    digitalWrite(buzz,LOW); 
+    delay(20); 
+ } 
+} 
