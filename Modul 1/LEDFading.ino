7032ba1b1fbbdeb438b670377eccd50471d1ea05
Hanindita Rifdah <37829261+deklutz@users.noreply.github.com>
2023-10-22 10:34:24 +0700

    Update LEDFading.ino

--- Modul 1/LEDFading.ino
+++ Modul 1/LEDFading.ino
@@ -6,11 +6,11 @@ pinMode(led, OUTPUT); // Set LED sebagai output
 } 
 
 void loop(){ 
-  for (i=0; i<=255; i+=5){ // Untuk menyalakan fading pada led
+  for (i=0; i<=255; i+=51){ // Untuk menyalakan fading pada led
     analogWrite(led,i); 
     delay(100); 
  } 
- for (i=255; i>=0; i-=5){  // Untuk meredupkan fading pada led
+ for (i=255; i>=0; i-=51){  // Untuk meredupkan fading pada led
   analogWrite(led,i); 
    delay(100); 
  } 
