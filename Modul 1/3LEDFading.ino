a01b7d51e6b12b5d29e0e0b3ff5d8478776e70f2
Hanindita Rifdah <37829261+deklutz@users.noreply.github.com>
2023-10-22 10:32:18 +0700

    Update 3LEDFading.ino

--- Modul 1/3LEDFading.ino
+++ Modul 1/3LEDFading.ino
@@ -8,33 +8,33 @@ pinMode(ledC, OUTPUT); // Set LED sebagai output
 } 
 
 void loop(){ 
-  for (i=0; i<=255; i+=5){ // Untuk menyalakan fading pada led
+  for (i=0; i<=255; i+=51){ // Untuk menyalakan fading pada led
     analogWrite(ledA,i); 
     delay(100); 
  } 
- for (i=255; i>=0; i-=5){  // Untuk meredupkan fading pada led
+ for (i=255; i>=0; i-=51){  // Untuk meredupkan fading pada led
   analogWrite(ledA,i); 
    delay(100); 
  } 
   
   delay (200); // Delay dari redup menuju penyalaan lampu selanjutnya
   
-  for (i=0; i<=255; i+=5){ // Untuk menyalakan fading pada led
+  for (i=0; i<=255; i+=51){ // Untuk menyalakan fading pada led
     analogWrite(ledB,i); 
     delay(100); 
  } 
- for (i=255; i>=0; i-=5){  // Untuk meredupkan fading pada led
+ for (i=255; i>=0; i-=51){  // Untuk meredupkan fading pada led
   analogWrite(ledB,i); 
    delay(100); 
  }
   
   delay (200); // Delay dari redup menuju penyalaan lampu selanjutnya
   
-  for (i=0; i<=255; i+=5){ // Untuk menyalakan fading pada led
+  for (i=0; i<=255; i+=51){ // Untuk menyalakan fading pada led
     analogWrite(ledC,i); 
     delay(100); 
  } 
- for (i=255; i>=0; i-=5){  // Untuk meredupkan fading pada led
+ for (i=255; i>=0; i-=51){  // Untuk meredupkan fading pada led
   analogWrite(ledC,i); 
    delay(100); 
  } 
