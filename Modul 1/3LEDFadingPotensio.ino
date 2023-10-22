5b7bb32839dfad15403b94da8a7774bafbb5e662
Hanindita Rifdah <37829261+deklutz@users.noreply.github.com>
2023-10-22 10:32:54 +0700

    Update 3LEDFadingPotensio.ino

--- Modul 1/3LEDFadingPotensio.ino
+++ Modul 1/3LEDFadingPotensio.ino
@@ -14,33 +14,33 @@ void loop(){
   data = analogRead(pot); // Data dibaca dari pin analog yang tersambung dengan potensiometer
   data = data / 4 // Membagi hasil data menjadi 4 agar skala diturunkan ke range 0-255, karena analogWrite hanya menerima nilai kecerahan LED dari 0 (mati)-255 (paling terang)
     
-  for (i=0; i<=255; i+=5){ // Untuk menyalakan fading pada led
+  for (i=0; i<=255; i+=51){ // Untuk menyalakan fading pada led
     analogWrite(ledA,i); 
     delay(100); 
  } 
- for (i=255; i>=0; i-=5){  // Untuk meredupkan fading pada led
+ for (i=255; i>=0; i-=51){  // Untuk meredupkan fading pada led
   analogWrite(ledA,i); 
    delay(100); 
  }
   
   delay (200);
   
-  for (i=0; i<=255; i+=5){ // Untuk menyalakan fading pada led
+  for (i=0; i<=255; i+=51){ // Untuk menyalakan fading pada led
     analogWrite(ledB,i); 
     delay(100); 
  } 
- for (i=255; i>=0; i-=5){  // Untuk meredupkan fading pada led
+ for (i=255; i>=0; i-=51){  // Untuk meredupkan fading pada led
   analogWrite(ledB,i); 
    delay(100); 
  }
   
   delay (200);
   
-  for (i=0; i<=255; i+=5){ // Untuk menyalakan fading pada led
+  for (i=0; i<=255; i+=51){ // Untuk menyalakan fading pada led
     analogWrite(ledC,i); 
     delay(100); 
  } 
- for (i=255; i>=0; i-=5){  // Untuk meredupkan fading pada led
+ for (i=255; i>=0; i-=51){  // Untuk meredupkan fading pada led
   analogWrite(ledC,i); 
    delay(100); 
  } 
